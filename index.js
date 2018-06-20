9b1c13596fdead9bb60319eb99d8af92e1d4c92b
Tya Raz <tyaraz.rocketmail@gmail.com>
2018-06-20 08:01:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ function updateObjectWithKeyAndValue (obj, key, value){
   return Object.assign({}, obj, {[key]:value})
 }
 
-updateObjectWithKeyAndValue(recipe, 'chocolate','1 cup')
+function destructivelyUpdateObjectWithKeyAndValur(obj, key, value){
+  
+  obj[key]=value
+  return obj
+}
 
+function destructivelyUpdateObjectWithKeyAndValur
